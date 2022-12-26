a09e40327ecec6f9ecfd127515ac544796475d49
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2022-12-26 12:03:28 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -104,3 +104,68 @@ export const projects = [
         url:"https://industrial-it-portfolio.vercel.app/",
     }
 ]
+
+
+
+export const BlogPPArticles= [
+    {
+        name: "Power Platform",
+        description:"Introduccion a las tecnologías del entorno Power Platform. PowerApps, Power Automate Power Bi y Virtual Agents",
+        image:"canvas.png",
+        link: "https://www.azurebrains.com/2022/01/31/powerplatform-perspectivas-futuro-parte1/" 
+    }
+    
+]
+
+
+
+export const BlogRPAArticles= [
+    {
+        name: "Power Platform",
+        description:"Introduccion a las tecnologías del entorno Power Platform. PowerApps, Power Automate Power Bi y Virtual Agents",
+        image:"canvas.png",
+        link: "https://www.azurebrains.com/2022/01/31/powerplatform-perspectivas-futuro-parte1/" 
+    }, {
+        name: "Manipulacion Dataverse(1)",
+        description:"Artículo sobre la manipulación de columnas tipo Lookup en Dataverse sobre canvas apps ",
+        image:"Dataverse.jpeg", 
+        link: ""
+    }
+    
+]
+
+
+
+export const BlogMLArticles= [
+    {
+        name: "Power Platform",
+        description:"Introduccion a las tecnologías del entorno Power Platform. PowerApps, Power Automate Power Bi y Virtual Agents",
+        image:"canvas.png",
+        link: "https://www.azurebrains.com/2022/01/31/powerplatform-perspectivas-futuro-parte1/" 
+    }, {
+        name: "Manipulacion Dataverse(1)",
+        description:"Artículo sobre la manipulación de columnas tipo Lookup en Dataverse sobre canvas apps ",
+        image:"Dataverse.jpeg", 
+        link: ""
+    }
+    
+]
+
+
+
+export const BlogIndustryArticles= [
+    {
+        name: "Power Platform",
+        description:"Introduccion a las tecnologías del entorno Power Platform. PowerApps, Power Automate Power Bi y Virtual Agents",
+        image:"canvas.png",
+        link: "https://www.azurebrains.com/2022/01/31/powerplatform-perspectivas-futuro-parte1/" 
+    }, {
+        name: "Manipulacion Dataverse(1)",
+        description:"Artículo sobre la manipulación de columnas tipo Lookup en Dataverse sobre canvas apps ",
+        image:"Dataverse.jpeg", 
+        link: ""
+    }
+    
+]
+
+
